99d924e4ee11422374f7fb9cd8c3b21ce076740c
lalaxaxa <andreylunew@gmail.com>
2025-05-20 18:19:31 +0300

    add KafkaListener

--- src/main/java/com/borisov/DZ_5/services/UserEventListenerService.java
+++ src/main/java/com/borisov/DZ_5/services/UserEventListenerService.java
@@ -1,19 +1,23 @@
 package com.borisov.DZ_5.services;
 
-import com.borisov.DZ_5.events.UserEvent;
+import borisov.core.UserChangedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserEventListenerService {
-    @KafkaListener(topics = "${topic.user-events}", containerFactory = "kafkaListenerContainerFactory")
-    public void onUserEvent(UserEvent event) {
-        String subject = "Уведомление от вашего сайта";
-        String body = switch (event.getOperation()) {
-            case CREATE -> "Здравствуйте! Ваш аккаунт на сайте успешно создан.";
-            case DELETE -> "Здравствуйте! Ваш аккаунт был удалён.";
-        };
-        System.out.println(body);
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+    @KafkaListener(topics = "${topic.user-events}")
+    public void onUserEvent(UserChangedEvent event) {
+//        String subject = "Уведомление от вашего сайта";
+//        String body = switch (event.getOperation()) {
+//            case CREATE -> "Здравствуйте! Ваш аккаунт на сайте успешно создан.";
+//            case DELETE -> "Здравствуйте! Ваш аккаунт был удалён.";
+//        };
+//        System.out.println(body);
         //emailSender.sendEmail(event.getEmail(), subject, body);
+        LOGGER.info("Receive new event "+ event.getEmail());
     }
 }
